ebd9ef2934cd5e37cbb157466a91231392f0b469
mihjovil <ma.caldas331@gmail.com>
2021-05-11 14:45:01 +0200

    :sparkles: The chatbot is working with input from console

--- train.py
+++ train.py
@@ -61,6 +61,6 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 
 sgd = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=2)
-model.save('chatbot_model.model')
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=2)
+model.save('chat_bot.h5', hist)
 print('Done')
